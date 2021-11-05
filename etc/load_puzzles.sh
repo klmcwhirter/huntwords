7641e8f2ff82809ba882c2366bb87aa65be5ce90
Kevin McWhirter <klmcwhirter@gmail.com>
2021-11-05 15:30:38 -0700

    Only reload if not 4 puzzle defs

--- etc/load_puzzles.sh
+++ etc/load_puzzles.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-python -m manager puzzle_load --file ./files/puzzles-all.json
+cd ~/src/github.com/klmcwhirter/python-projects/huntwords
 
-for p in Animals Bible Flowers Fruit;do for ct in 1 2 3 4 5;do python -m manager puzzleboard_consume --name $p;sleep 2;done;done
+source ./venv/bin/activate
+
+count=$(python -m manager puzzles | awk -F '=' '/^text=/ { print $2 }' | jq -c 'length')
+echo count=${count}
+
+if [ "$1" != "reload" -o ${count} -ne 4 ]
+then
+    python -m manager puzzleboards_clear
+
+    python -m manager puzzle_load --file ./files/puzzles-all.json
+
+    for p in Animals Bible Flowers Fruit;do for ct in 1 2 3 4 5;do python -m manager puzzleboard_consume --name $p;sleep 2;done;done
+fi
